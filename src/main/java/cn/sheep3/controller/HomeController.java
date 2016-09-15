ae4915973960965f295c658773f8937cebdbce5f
sheepmen <xuxinhome@126.com>
2016-09-15 23:38:18 +0800

    Dev: add editormd.js
    Signed-off-by: sheepmen <xuxinhome@126.com>

--- src/main/java/cn/sheep3/controller/HomeController.java
+++ src/main/java/cn/sheep3/controller/HomeController.java
@@ -15,4 +15,6 @@ public class HomeController {
         return "/index";
     }
 
+
+
 }
--- src/main/java/cn/sheep3/controller/UserController.java
+++ src/main/java/cn/sheep3/controller/UserController.java
@@ -1,13 +1,10 @@
 package cn.sheep3.controller;
 
-import cn.sheep3.entity.Post;
-import cn.sheep3.entity.User;
 import cn.sheep3.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Created by sheep3 on 16-9-14.
@@ -18,15 +15,26 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
-    @RequestMapping("/get-by-name")
-    @ResponseBody
-    public String getByEmail(@RequestParam("name") String name) {
-        String userId;
-        User user = userRepository.findByUserLogin(name);
-        if (user != null) {
-            userId = String.valueOf(user.getId());
-            return "The user id is: " + userId;
-        }
-        return "user " + name + " is not exist.";
+    @RequestMapping("/admin")
+    public String getByEmail() {
+        return "/admin/index";
     }
+
+    @RequestMapping("/admin/edit")
+    public String editPost() {
+        return "/admin/edit";
+    }
+
+
+
+    @RequestMapping(path = "/login",method = {RequestMethod.GET})
+    public String login() {
+        return "/admin/login";
+    }
+
+    @RequestMapping(path = "/login",method = {RequestMethod.POST})
+    public String signIn(){
+        return "/admin/index";
+    }
+
 }
